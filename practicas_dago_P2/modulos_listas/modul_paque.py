5332eba97aa38661523f7c0f833ed40c3f6ee117
liraroachosaulosebastian-ai <liraroachosaulosebastian@gmail.com>
2026-06-14 13:24:03 -0600

    Create modul_paque.py

---
+++ practicas_dago_P2/modulos_listas/modul_paque.py
@@ -0,0 +1,9 @@
+# Un módulo es simplemente un archivo con extensión .py que contiene código de Python (funciones, clases, variables, etc.).
+
+# Un paquete es una carpeta que contiene varios módulos (archivos .py) y un archivo especial llamado __init__.py, que le indica a Python que esa carpeta debe tratarse como un paquete.
+
+edad=int(input("Escribe la edad: "))
+
+def SolicitarEdad():
+    edad=int(input("Edad: "))
+    return edad 
