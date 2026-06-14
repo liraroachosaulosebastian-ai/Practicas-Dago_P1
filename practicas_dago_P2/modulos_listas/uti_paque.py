b58af75892ad795c057fe1bfb88a15e814229a5f
liraroachosaulosebastian-ai <liraroachosaulosebastian@gmail.com>
2026-06-14 13:24:22 -0600

    Create uti_paque.py

---
+++ practicas_dago_P2/modulos_listas/uti_paque.py
@@ -0,0 +1,8 @@
+from modul_paque import modulos,modulo_paquete
+
+modulos.borrarpantalla()
+
+nom,ape=modulos.funcion2()
+edad=modulo_paquete.SolicitarEdad()
+
+print(f"Nombre: {nom}\nApellidos:{ape}\Edad: {edad}")
