95ed35d2a27360c753d865b61cd7c051852220b7
liraroachosaulosebastian-ai <liraroachosaulosebastian@gmail.com>
2026-06-14 10:17:44 -0600

    Create tuples_listas.py

---
+++ practicas_dago_P1/practicas_dago_P2/tuple/tuples_listas.py
@@ -0,0 +1,24 @@
+"""   
+
+  Las tuplas se utilizan para almacenar varios elementos en una sola variable.
+
+   Una tupla es una colección ordenada e inmutable .
+
+   Las tuplas se escriben entre paréntesis.
+
+
+"""
+print("\033c")
+
+paises=("México","Brasil","España","Canadá","Canadá")
+print(paises)
+
+posicion=paises.index("España")
+print(posicion)
+
+varios=(True,"UTO",33,3.14,33)
+print(varios)
+print(varios[1])
+cuantos=varios.count(33)
+
+print(cuantos)
