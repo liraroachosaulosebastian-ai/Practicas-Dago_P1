43ef66098df6725c9b2a4f0359f6c9bc2c60a9e1
liraroachosaulosebastian-ai <liraroachosaulosebastian@gmail.com>
2026-06-18 15:48:46 -0600

    Create preparacion_eje_1.py

---
+++ preparacion_examen/preparacion_eje_1.py
@@ -0,0 +1,15 @@
+def estudianteCalificacion(alumno,nota):
+    
+    return()
+
+opc="S"
+alumno_listas=[]
+alumno_dict={}
+contador=0
+
+while opc=="S":
+    al=input("ingreasa el nombre del alumno:").upper().strip()
+    calif=float(input("ingresa la nota del alumno:"))
+    evaluacion=estudianteCalificacion(alumno,nota)
+
+
